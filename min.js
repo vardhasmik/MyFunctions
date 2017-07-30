5359d01a052cff31baec6a4eaa1de429f1f059fa
Anna Vardanyan <vardhasmik@gmail.com>
2017-07-30 20:27:33 +0400

    Add all .js files

---
+++ min.js
@@ -0,0 +1,28 @@
+min=(a,b)=>{
+  if(a<b) return a
+  return b
+}
+
+console.log(min(5,-10));
+
+isEven=(a)=>{
+  if(a%2===0) return true
+  return false
+}
+
+console.log(isEven(-8))
+
+
+countBs=(string, symbol)=>{
+  let count=0
+  for(let i=0;i<string.length;i++)
+  {
+    if(string[i]==symbol)
+    {
+      count+=1;
+    }
+  }
+  return count
+}
+
+console.log(countBs("hello-hello", 'o'))
